d95f1b41089e27c6b557493765c6d406c5431e82
Rhett Harrison <reharri7@asu.edu>
2021-08-05 22:42:44 -0700

    Minimum Viable Product. Created a backend with an index route and dynamic route to search the database. Connected to Firebase Firestore Database

---
+++ server.js
@@ -0,0 +1,58 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const enforce = require('express-sslify');
+const firebase = require('firebase');
+const path = require('path');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+// Firebase
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const urlCollection = 'urls';
+
+// Express
+const app = express();
+const port = process.env.PORT || 3001;
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+// POST request for creating urls?
+
+app.get('/:code', async (req, res) => {
+    const code = req.params.code;
+    const query = await db.collection(urlCollection).where("code", "==", code);
+
+    query.onSnapshot((data) => {
+        if(data.empty){
+            res.status(404).render('404');
+            return;
+        }
+        let url = data.docs[0].data().url;
+        res.status(301).redirect(`https://${ url }`);
+    });
+
+});
+
+app.listen(port, error => {
+    if(error) throw error;
+    console.log('Server is running on port ' + port)
+});
